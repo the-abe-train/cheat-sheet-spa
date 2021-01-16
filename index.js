4df543f2d62b48d9c4560e6e13c91b0dd7ab94fe
the-abe-train <abraham.train@gmail.com>
2021-01-15 22:01:45 -0500

    Transferred functions to zoom.js

--- index.js
+++ index.js
@@ -26,23 +26,23 @@ tr.nodes([]); // need a default in transformer, so empty array
 layer.add(tr);
 
 // Write helpful messages while testing!
-const text = new Konva.Text({
-    x: 10,
-    y: 10,
-    fontFamily: 'Calibri',
-    fontSize: 20,
-    text: '',
-    fill: 'black',
-});
+// const text = new Konva.Text({
+//     x: 10,
+//     y: 10,
+//     fontFamily: 'Calibri',
+//     fontSize: 20,
+//     text: '',
+//     fill: 'black',
+// });
 
-function writeMessage(message) {
-    text.text(message);
-    layer.draw();
-}
-function printCoords() {
-    const { x, y } = this.position();
-    writeMessage(`${this.name()} x: ${x}, y: ${y}`);
-}
+// function writeMessage(message) {
+//     text.text(message);
+//     layer.draw();
+// }
+// function printCoords() {
+//     const { x, y } = this.position();
+//     writeMessage(`${this.name()} x: ${x}, y: ${y}`);
+// }
 
 
 // Step 3: Create shapes
@@ -58,7 +58,7 @@ let rect1 = new Konva.Rect({
     cornerRadius: 12,
     draggable: true
 })
-rect1.on('mouseup', printCoords);
+// rect1.on('mouseup', printCoords);
 
 let rect2 = new Konva.Rect({
     x: 200,
@@ -72,7 +72,7 @@ let rect2 = new Konva.Rect({
     cornerRadius: 12,
     draggable: true
 })
-rect2.on('mouseup', printCoords);
+// rect2.on('mouseup', printCoords);
 
 // Create new group and add layers to group
 const group = new Konva.Group({
@@ -84,62 +84,21 @@ group.add(rect1, rect2);
 layer.add(group);
 
 // Step 4: Draw layer
-layer.add(text);
+// layer.add(text);
 layer.draw();
 
-// Dynamic resize canvas when resizing window
-function fitStageIntoParentContainer() {
-
-    const pageHeight = window.innerHeight;
-    const pageWidth = window.innerWidth;
-    const sidebarWidth = document.querySelector(".sidenav").clientWidth;
-
-    const containerWidth = pageWidth - sidebarWidth;
-    const containerHeight = pageHeight;
-
-    stage.width(containerWidth);
-    stage.height(containerHeight);
-    stage.draw();
-}
-
-fitStageIntoParentContainer()
-window.addEventListener('resize', fitStageIntoParentContainer);
-
-// Zoom with scroll wheel
-const scaleBy = 1.03;
-stage.on('wheel', (e) => {
-    e.evt.preventDefault();
-    const oldScale = stage.scaleX();
-
-    const pointer = stage.getPointerPosition();
-
-    const mousePointTo = {
-        x: (pointer.x - stage.x()) / oldScale,
-        y: (pointer.y - stage.y()) / oldScale,
-    };
-
-    const newScale =
-        e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy;
-
-    stage.scale({ x: newScale, y: newScale });
-
-    const newPos = {
-        x: pointer.x - mousePointTo.x * newScale,
-        y: pointer.y - mousePointTo.y * newScale,
-    };
-    stage.position(newPos);
-    stage.batchDraw();
-});
 
 
 
+
+
+
+// Selecting shapes
 // Selection rectangle
 // const selectionRectangle = new Konva.Rect({
 //     fill: 'rgba(0,0,255,0.5)',
 // });
 // layer.add(selectionRectangle);
-
-// Selecting shapes
 stage.on('click tap', e => {
     // console.log(tr)
     // console.log(tr.nodes())
@@ -181,119 +140,4 @@ stage.on('click tap', e => {
     }
     layer.draw();
 
-})
-
-// Recentre screen
-document.querySelector('#centre').addEventListener('click', (e) => {
-
-    function recentre() {
-
-        const stageCentre = {
-            x: stage.x() / 2,
-            y: stage.y() / 2
-        }
-
-        // Get midpoint of all shapes
-        const midpoints = group.getChildren().map(node => {
-            const { x, y } = node.position();
-            const { width, height } = node.size();
-            return {
-                x: x + (width / 2),
-                y: y + (height / 2)
-            }
-        })
-
-        // Get geometric centre of all shapes
-        const geoCentre = midpoints.reduce((acc, curr, idx, src) => {
-            acc.x += curr.x / src.length;
-            acc.y += curr.y / src.length;
-            return acc
-        }, { x: 0, y: 0 })
-
-        // Find centre of screen
-        const screenCentre = {
-            x: stage.width() / 2,
-            y: stage.height() / 2
-        }
-
-        // Get vector from geo centre -> screen centre
-        const vector = {
-            x: screenCentre.x - geoCentre.x,
-            y: screenCentre.y - geoCentre.y
-        }
-
-        // Move all shapes by vector
-        group.getChildren().forEach(node => node.move(vector));
-    }
-
-    function checkBounds() {
-        const scale = stage.scaleX();
-        const edges = group.getChildren().reduce((acc, node) => {
-            return {
-                top: Math.min(acc.top, (stage.y() + node.y()) * scale),
-                right: Math.max(acc.right, (stage.x() + node.x() + node.width()) * scale),
-                bottom: Math.max(acc.bottom, (stage.y() + node.y() + node.height()) * scale),
-                left: Math.min(acc.left, (stage.x() + node.x()) * scale)
-            }
-        }, {
-            top: (stage.y() + stage.height()) * scale,
-            right: (stage.x() + stage.width()) * scale,
-            bottom: (stage.y() + stage.height()) * scale,
-            left: (stage.x() + stage.width()) * scale
-        })
-        const outOfBounds = [
-            edges.top < 0,
-            edges.right > window.innerWidth,
-            edges.bottom > window.innerHeight,
-            edges.left < 0
-        ].some(bound => bound === true)
-
-        return outOfBounds
-    }
-
-
-    // Scale stage until shapes are within bounds
-    function zoom(scaleBy) {
-        const oldScale = stage.scaleX();
-
-        const pointer = {
-            x: stage.x() + stage.width() / 2,
-            y: stage.y() + stage.height() / 2,
-        }
-
-        const mousePointTo = {
-            x: (pointer.x - stage.x()) / oldScale,
-            y: (pointer.y - stage.y()) / oldScale,
-        };
-
-        const newScale = oldScale * scaleBy;
-
-        stage.scale({ x: newScale, y: newScale });
-
-        const newPos = {
-            x: pointer.x - mousePointTo.x * newScale,
-            y: pointer.y - mousePointTo.y * newScale,
-        };
-        stage.position(newPos);
-        stage.batchDraw();
-    }
-
-    // Reset basic canvas positions and vectors
-    stage.scale({ x: 1, y: 1 })
-    stage.position({ x: 0, y: 0 })
-    group.position({ x: 0, y: 0 })
-
-    // Initial recentre
-    recentre();
-
-    // Zoom until all shapes within bounds
-    let outOfBounds = checkBounds();
-    while (outOfBounds) {
-        zoom(0.9);
-        recentre();
-        outOfBounds = checkBounds();
-    }
-
-    // Redraw layer
-    layer.draw();
 })
