7e90781af9c4663d5d8ad4f08c005c0d467f7975
the-abe-train <abraham.train@gmail.com>
2021-02-14 21:11:18 -0500

    Reformatted

--- src/utils/zoom.js
+++ src/utils/zoom.js
@@ -1,152 +1,152 @@
-import {stage, layer, tr, allArrows, allNodes} from './canvasSetup'
+import { stage, layer, tr, allArrows, allNodes } from './canvasSetup'
 
 // Dynamic resize canvas when resizing window
 export function fitZoom() {
 
-    const pageHeight = window.innerHeight;
-    const pageWidth = window.innerWidth;
-    const sidebarWidth = document.querySelector(".sidenav").clientWidth;
+  const pageHeight = window.innerHeight;
+  const pageWidth = window.innerWidth;
+  const sidebarWidth = document.querySelector(".sidenav").clientWidth;
 
-    const containerWidth = pageWidth - sidebarWidth;
-    const containerHeight = pageHeight;
+  const containerWidth = pageWidth - sidebarWidth;
+  const containerHeight = pageHeight;
 
-    stage.width(containerWidth);
-    stage.height(containerHeight);
-    stage.draw();
+  stage.width(containerWidth);
+  stage.height(containerHeight);
+  stage.draw();
 }
 
 
 
 // Zoom function
 export function zoom(direction, point, scaleBy = 1.2) {
-    const oldScale = stage.scaleX();
+  const oldScale = stage.scaleX();
 
-    const pointVector = {
-        x: (point.x - stage.x()) / oldScale,
-        y: (point.y - stage.y()) / oldScale,
-    };
+  const pointVector = {
+    x: (point.x - stage.x()) / oldScale,
+    y: (point.y - stage.y()) / oldScale,
+  };
 
-    const newScale = direction === 'in' ? oldScale * scaleBy : oldScale / scaleBy;
+  const newScale = direction === 'in' ? oldScale * scaleBy : oldScale / scaleBy;
 
-    stage.scale({ x: newScale, y: newScale });
+  stage.scale({ x: newScale, y: newScale });
 
-    const newPos = {
-        x: point.x - pointVector.x * newScale,
-        y: point.y - pointVector.y * newScale,
-    };
-    stage.position(newPos);
-    stage.batchDraw();
+  const newPos = {
+    x: point.x - pointVector.x * newScale,
+    y: point.y - pointVector.y * newScale,
+  };
+  stage.position(newPos);
+  stage.batchDraw();
 }
 
 // Zoom with scroll wheel
 export const scrollZoom = e => {
-    e.evt.preventDefault();
-    const direction = e.evt.deltaY > 0 ? 'out' : 'in'
-    zoom(direction, stage.getPointerPosition(), 1.03)
+  e.evt.preventDefault();
+  const direction = e.evt.deltaY > 0 ? 'out' : 'in'
+  zoom(direction, stage.getPointerPosition(), 1.03)
 }
 
 
 // Get centre of screen
 export function stageCentre() {
-    // Get the relative centre of the stage
-    return {
-        x: stage.width() / 2,
-        y: stage.height() / 2,
-    }
+  // Get the relative centre of the stage
+  return {
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+  }
 }
 
 // Recentre screen
 export const recentreZoom = (e) => {
 
-    function recentre() {
+  function recentre() {
 
-        // Get midpoint of all shapes
-        const midpoints = allNodes.getChildren().map(node => {
-            const { x, y } = node.position();
-            const { width, height } = node.size();
-            return {
-                x: x + (width / 2),
-                y: y + (height / 2)
-            }
-        })
+    // Get midpoint of all shapes
+    const midpoints = allNodes.getChildren().map(node => {
+      const { x, y } = node.position();
+      const { width, height } = node.size();
+      return {
+        x: x + (width / 2),
+        y: y + (height / 2)
+      }
+    })
 
-        // Get centre of mass of all shapes
-        // Geometric median might be better here, but hard to implement
-        const geoCentre = midpoints.reduce((acc, curr, idx, src) => {
-            acc.x += curr.x / src.length;
-            acc.y += curr.y / src.length;
-            return acc
-        }, { x: 0, y: 0 })
+    // Get centre of mass of all shapes
+    // Geometric median might be better here, but hard to implement
+    const geoCentre = midpoints.reduce((acc, curr, idx, src) => {
+      acc.x += curr.x / src.length;
+      acc.y += curr.y / src.length;
+      return acc
+    }, { x: 0, y: 0 })
 
-        // Find absolute centre of canvas
-        const centre = stageCentre();
+    // Find absolute centre of canvas
+    const centre = stageCentre();
 
-        // Get vector from geo centre -> screen centre
-        const vector = {
-            x: centre.x - geoCentre.x,
-            y: centre.y - geoCentre.y
-        }
-
-        // Move all shapes by vector
-        allNodes.getChildren().forEach(node => node.move(vector));
+    // Get vector from geo centre -> screen centre
+    const vector = {
+      x: centre.x - geoCentre.x,
+      y: centre.y - geoCentre.y
     }
 
-    function checkBounds() {
-        const scale = stage.scaleX();
+    // Move all shapes by vector
+    allNodes.getChildren().forEach(node => node.move(vector));
+  }
 
-        // Bounds of the visible stage
-        const bounds = document.querySelector('#konva-holder').getBoundingClientRect();
+  function checkBounds() {
+    const scale = stage.scaleX();
 
-        // Get absolute bounds of all nodes ()
-        const edges = allNodes.getChildren().reduce((acc, node) => {
-            return {
-                top: Math.min(acc.top, bounds.top + stage.y() + scale * node.y()),
-                right: Math.max(acc.right, bounds.left + stage.x() + scale * (node.x() + node.width())),
-                bottom: Math.max(acc.bottom, bounds.top + scale * (node.y() + node.height())),
-                left: Math.min(acc.left, bounds.left + stage.x() + scale * node.x())
-            }
-        }, {  // Start with middle of stage
-            top: stageCentre().y,
-            right: stageCentre().x,
-            bottom: stageCentre().y,
-            left: stageCentre().x
-        })
+    // Bounds of the visible stage
+    const bounds = document.querySelector('#container').getBoundingClientRect();
 
-        const outOfBounds = [
-            edges.top < bounds.top,
-            edges.right > bounds.right,
-            edges.bottom > bounds.bottom,
-            edges.left < bounds.left
-        ].some(bound => bound === true)
+    // Get absolute bounds of all nodes ()
+    const edges = allNodes.getChildren().reduce((acc, node) => {
+      return {
+        top: Math.min(acc.top, bounds.top + stage.y() + scale * node.y()),
+        right: Math.max(acc.right, bounds.left + stage.x() + scale * (node.x() + node.width())),
+        bottom: Math.max(acc.bottom, bounds.top + scale * (node.y() + node.height())),
+        left: Math.min(acc.left, bounds.left + stage.x() + scale * node.x())
+      }
+    }, {  // Start with middle of stage
+      top: stageCentre().y,
+      right: stageCentre().x,
+      bottom: stageCentre().y,
+      left: stageCentre().x
+    })
 
-        // console.log(JSON.stringify(edges));
+    const outOfBounds = [
+      edges.top < bounds.top,
+      edges.right > bounds.right,
+      edges.bottom > bounds.bottom,
+      edges.left < bounds.left
+    ].some(bound => bound === true)
 
-        return outOfBounds
-    }
+    // console.log(JSON.stringify(edges));
 
-    // Reset basic canvas positions and vectors
-    stage.scale({ x: 1, y: 1 })
-    stage.position({ x: 0, y: 0 })
-    allNodes.position({ x: 0, y: 0 })
+    return outOfBounds
+  }
 
-    // Initial recentre
+  // Reset basic canvas positions and vectors
+  stage.scale({ x: 1, y: 1 })
+  stage.position({ x: 0, y: 0 })
+  allNodes.position({ x: 0, y: 0 })
+
+  // Initial recentre
+  recentre();
+
+  // Zoom until all shapes within bounds
+  let outOfBounds = checkBounds();
+  while (outOfBounds) {
+    zoom('out', stageCentre());
     recentre();
+    outOfBounds = checkBounds();
+  }
 
-    // Zoom until all shapes within bounds
-    let outOfBounds = checkBounds();
-    while (outOfBounds) {
-        zoom('out', stageCentre());
-        recentre();
-        outOfBounds = checkBounds();
-    }
+  // Redraw arrows
+  // const arrows = allArrows.getChildren(node => node.getClassName() === "Arrow")
 
-    // Redraw arrows
-    // const arrows = allArrows.getChildren(node => node.getClassName() === "Arrow")
+  for (let arrow of allArrows.getChildren()) {
+    arrow.update();
+  }
 
-    for (let arrow of allArrows.getChildren()) {
-        arrow.update();
-    }
-
-    // Redraw layer
-    layer.draw();
+  // Redraw layer
+  layer.draw();
 }
